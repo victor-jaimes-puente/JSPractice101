acad6e21d31887d9ae731b18d9d392e1a6e6c94b
VictorJP <victor.jaimes.puente@gmail.com>
2020-07-05 01:55:09 -0500

    @5

--- js/101JS.js
+++ js/101JS.js
@@ -30,31 +30,37 @@ function addToDone(message) {
 // Example problem setup: Create a variable named doingJSRightNow and assign it the boolean true.
 // The line below creates the variable named doingJSRightNow and assigns the boolean value true
 // To complete Exercise #0, uncomment the following line of JS
-// var doingJSRightNow = true
+var doingJSRightNow = true
 
 // The lines below will test your answer. If you see an error, then it means that your answer is incorrect or incomplete.
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Not_defined to understand this error message.
+//vjp
+    
 assert(doingJSRightNow, true, "Exercise 0"); 
 
 
 //  Exercise 1
 // On the line below, create a variable named onMarsRightNow and assign it the boolean value of false
 // For more on variables, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables
-
-assert(onMarsRightNow, false, "Exercise 1");
-addToDone("Exercise 1 is correct.");
+//vjp
+    var onMarsRightNow = false;
+    assert(onMarsRightNow, false, "Exercise 1");
+    addToDone("Exercise 1 is correct.");
 
 // Exercise 2
 // For more on arrays, see https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
 // Create a variable named fruits and assign it an array of strings containing the following fruits.
 // mango, banana, guava, kiwi, and strawberry.
-
+//vjp
+    var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
 assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry"], "Exercise 2");
 addToDone("Exercise 2 is correct.");
 
 // Exercise 3
 // Create a variable named vegetables and assign it an array of strings containing the following vegetables.
 // eggplant, broccoli, carrot, cauliflower, and zucchini
+//vjp
+    var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"]
 
 assert(vegetables, ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"], "Exercise 3");
 addToDone("Exercise 3 is correct.");
@@ -62,7 +68,8 @@ addToDone("Exercise 3 is correct.");
 
 // Exercise 4
 // Create a variable named numbers and assign it an array of numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-
+//vjp
+    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 assert(numbers, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "Exercise 4");
 addToDone("Exercise 4 is correct.");
 
@@ -70,14 +77,16 @@ addToDone("Exercise 4 is correct.");
 // Exercise 5
 // Add the string "tomato" to the end of the fruits array. 
 // *Hint* Recommend finding and using a built-in JS operation to add to an array rather than recreating the array.
-
+//vjp 
+    var fruits = ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"]
 assert(fruits, ["mango", "banana", "guava", "kiwi", "strawberry", "tomato"], "Exercise 5");
 addToDone("Exercise 5 is correct");
 
 // Exercise 6
 // add the string "tomato" onto the end of the vegetables array. 
 // Recommend using the built-in JS operation to add to an array.
-
+//vjp
+    
 
 assert(vegetables,["eggplant", "broccoli", "carrot", "cauliflower", "zucchini", "tomato"], "Exercise 6");
 addToDone("Exercise 6 is correct")
